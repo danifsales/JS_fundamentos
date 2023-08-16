2ab21a93aa533e69220a281369f65ba7cedc1a3c
danifsales <danielafsales@gmail.com>
2023-08-16 18:02:16 -0300

    códigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+//booleanos
+
+// conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString);
+console.log(numero + numeroString);
+// Função Number()
+// Função String()
+console.log(numero + Number(numeroString)); // ou é possível colocar diretamente Number na variável
+
+
+// conversão explícita
